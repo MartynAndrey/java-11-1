03d58b516a63cb4adced6e68a25796c76d307ef2
Andrey Martyn <martynandrey@rambler.ru>
2021-12-18 21:54:48 +0600

    Realized PosterManager class with metods (plus Movie class)

--- src/main/java/ru/netology/poster/Movie.java
+++ src/main/java/ru/netology/poster/Movie.java
@@ -1,4 +1,17 @@
 package ru.netology.poster;
 
 public class Movie {
+    private int id;
+    private String name;
+    private String kind;
+    private String url;
+    private String image;
+
+    public Movie(int id, String name, String kind, String url, String image) {
+        this.id = id;
+        this.name = name;
+        this.kind = kind;
+        this.url = url;
+        this.image = image;
+    }
 }
--- src/main/java/ru/netology/poster/PosterManager.java
+++ src/main/java/ru/netology/poster/PosterManager.java
@@ -1,4 +1,48 @@
 package ru.netology.poster;
 
 public class PosterManager {
+    private Movie[] movies = new Movie[0];
+    private int recentMoviesLimit = 10;
+
+    public PosterManager() {}
+
+    public PosterManager(int recentMoviesLimit) {
+        if (recentMoviesLimit > 0) {
+            this.recentMoviesLimit = recentMoviesLimit;
+        }
+    }
+
+    public int getRecentMoviesLimit() {
+        return this.recentMoviesLimit;
+    }
+
+    public Movie[] getMovies() {
+        return this.movies;
+    }
+
+    public void addMovieToPoster(Movie movie) {
+        Movie[] newMovies = new Movie[movies.length + 1];
+
+        System.arraycopy(this.movies, 0, newMovies, 0, movies.length);
+
+        newMovies[newMovies.length - 1] = movie;
+
+        this.movies = newMovies;
+    }
+
+    public Movie[] getRecentMovies() {
+        Movie[] recentMovies = null;
+
+        if (this.recentMoviesLimit <= this.movies.length) {
+            recentMovies = new Movie[this.recentMoviesLimit];
+        } else {
+            recentMovies = new Movie[this.movies.length];
+        }
+
+        for (int i = 0; i < recentMovies.length; i++) {
+            recentMovies[i] = this.movies[this.movies.length - 1 - i];
+        }
+
+        return recentMovies;
+    }
 }
